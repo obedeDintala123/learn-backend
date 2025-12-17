6bf3c65c64d0e52f4bb35b866500f35f1be1f073
obedeDintala123 <obededintala@gmail.com>
2025-12-17 17:09:12 +0100

    feat: add hello world

--- myApp.js
+++ myApp.js
@@ -3,6 +3,9 @@ let app = express();
 
 let absolutePath = __dirname + "/views/index.html"
 
+//add middleware
+app.use("/public", express.static(__dirname + "/public"))
+
 app.get("/", (req, res) => {
     res.sendFile(absolutePath);
 })
